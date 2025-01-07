9f170375c4d78920301bb257dd1b5d9bc9eee8f3
Nicolas Chagnet <nchagnet@protonmail.com>
2025-01-07 09:11:03 +0100

    updated figure making

--- src/figure.py
+++ src/figure.py
@@ -19,6 +19,7 @@ class PredictionFigure:
         y_actual = pd.concat([self.output_prediction["train_actual"], self.output_prediction["future_actual"]])
         y_pred = pd.concat([self.output_prediction["train_pred"], self.output_prediction["future_pred"]])
         y_forecast = self.output_prediction["future_forecast"]
+        y_last_week = y_actual.shift(7 * 24)
 
         total_hours_prediction = (
             self.output_prediction["future_pred"].index.max()
@@ -37,12 +38,37 @@ class PredictionFigure:
         forecast_legend = (
             'Total system load (ENTSOE one-day forecast)<br>'
             f"Entsoe forecast (24h): MAE = {self.output_prediction['metrics_forecast_one_day']['mae']:.0f}, MAPE = {self.output_prediction['metrics_forecast_one_day']['mape']:.2f}<br>"
-            f"Entsoe forecast ({total_hours_prediction:.0f}h): MAE = {self.output_prediction['metrics_forecast']['mae']:.0f}, MAPE = {self.output_prediction['metrics_forecast']['mape']:.2f}"
+            f"Entsoe forecast ({total_hours_prediction:.0f}h): MAE = {self.output_prediction['metrics_forecast']['mae']:.0f}, MAPE = {self.output_prediction['metrics_forecast']['mape']:.2f}<br>"
         )
+        last_week_legend = "Total system load (actual, last week)"
 
         self.fig.add_trace(go.Scatter(x=y_actual.index, y=y_actual, mode="lines+markers", name=actual_legend))
         self.fig.add_trace(go.Scatter(x=y_pred.index, y=y_pred, mode="lines+markers", name=pred_legend))
         self.fig.add_trace(go.Scatter(x=y_forecast.index, y=y_forecast, mode="lines+markers", name=forecast_legend))
+        self.fig.add_trace(
+            go.Scatter(
+                x=y_last_week.index,
+                y=y_last_week,
+                mode="lines+markers",
+                line=dict(dash="dash"),
+                name=last_week_legend,
+                # opacity=0.6,
+            )
+        )
+        self.fig.update_layout(
+            autosize=False,
+            width=1400,
+            height=800,
+            legend=dict(
+                # itemwidth=100,
+                yref="paper",
+                orientation="h",  # show entries horizontally
+                xanchor="center",  # use center of legend as anchor
+                x=0.5,
+                y=-0.15,
+            ),
+            xaxis=dict(automargin="height"),
+        )
         self.fig.update_layout(legend_valign="top")
         max_range = y_actual.index.max()
         min_range = end_training - pd.Timedelta(days=1)
@@ -63,7 +89,7 @@ class PredictionFigure:
             text="End of training period",
             showarrow=False,
             yref="paper",
-            y=1.1,
+            y=1.05,
         )
 
     def write_to_file(self) -> None:
